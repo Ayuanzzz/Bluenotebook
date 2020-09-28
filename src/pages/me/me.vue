7fd2133d6d1c25417d0546fc59176d57e2d39425
huhu <1052833858@qq.com>
2020-09-29 07:18:28 +0800

    0929

---
+++ src/pages/me/me.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <div>
+      <p>与<span><img src="/static/images/p2.png"/></span>在一起</p>
+      <p>第12天</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
