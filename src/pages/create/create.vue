77f4d01d545649ab8a40bba8cc5bafd4707dde20
huhu <1052833858@qq.com>
2021-05-09 10:44:18 +0800

    2021.5.9

---
+++ src/pages/create/create.vue
@@ -0,0 +1,129 @@
+<template>
+  <div class="container">
+    <navBar :name="navName"></navBar>
+    <div class="profile">
+      <p>选择头像</p>
+      <swiper class="swiper" indicator-dots="true" circular="true">
+        <block v-for="(item, index) in movies" :key="index">
+          <swiper-item>
+            <image :src="item" class="slide-image" mode="aspectFill" />
+          </swiper-item>
+        </block>
+      </swiper>
+    </div>
+    <div class="nickName">
+      <p>昵称</p>
+      <form catchsubmit="formSubmit" catchreset="formReset">
+        <input
+          class="weui-input"
+          name="input"
+          placeholder="最多输入6个字哦"
+          maxlength="6"
+        />
+        <div class="btn-area">
+          <button type="primary" formType="submit" @click="formSubmit(e)">
+            创建
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import navBar from "@/components/navBar";
+export default {
+  components: {
+    navBar,
+  },
+  data() {
+    return {
+      movies: [
+        "/static/images/bigicon/iconfinder__deer_.png",
+        "/static/images/bigicon/iconfinder_bug--animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_Food_C_.png",
+        "/static/images/bigicon/iconfinder_frog-animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_icon_animal_cachorro_.png",
+        "/static/images/bigicon/iconfinder_icon_animal_pato_.png",
+        "/static/images/bigicon/iconfinder_Inkcontober_Screech_Psyduck_.png",
+        "/static/images/bigicon/iconfinder_octopus-sea--animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_parrot-bird--animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_pig-animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_pinguin-animal-pet-wild-domestic_.png",
+        "/static/images/bigicon/iconfinder_sheep-animal-pet-wild-domestic_.png",
+      ],
+    };
+  },
+  methods: {
+    formSubmit(e) {
+      console.log("form发生了submit事件，携带数据为：", e);
+    },
+  },
+  created() {},
+};
+</script>
+
+<style lang='scss'>
+.container {
+  width: 100%;
+}
+p {
+  position: absolute;
+  left: 42px;
+  font-size: 17px;
+  font-weight: bold;
+  color: #4378db;
+}
+.profile {
+  position: relative;
+  width: 100%;
+  height: 300px;
+  p {
+    top: 27px;
+  }
+  swiper {
+    padding-top: 75px;
+    width: 250px;
+    height: 200px;
+    margin: 0 auto;
+    image {
+      display: block;
+      width: 150px;
+      height: 150px;
+      margin: 0 auto;
+    }
+  }
+}
+.nickName {
+  position: relative;
+  width: 100%;
+  p {
+    top: 27px;
+  }
+  form {
+    position: relative;
+    width: 375px;
+    height: 200px;
+    top: 30px;
+    input {
+      border-bottom: 1px solid #000000;
+      position: absolute;
+      width: 150px;
+      height: 40px;
+      left: 42px;
+      top: 30px;
+    }
+    .btn-area {
+      position: relative;
+      top: 150px;
+      height: 50px;
+      width: 100%;
+      text-align: center;
+      button {
+        display: block;
+        width: 200px;
+      }
+    }
+  }
+}
+</style>
